32f083a925539be0642b35d99f2ad3834f9204b0
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-05-02 09:14:16 +0900

    Initial Commit

---
+++ Shared/TestimonialsGridSectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestimonialsGridSectionApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/05/02.
+//
+
+import SwiftUI
+
+@main
+struct TestimonialsGridSectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
